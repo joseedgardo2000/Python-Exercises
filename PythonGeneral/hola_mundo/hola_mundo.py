8b270685fc6319884ea605a4d6c27ed823ea97dd
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-09-01 21:03:55 -0500

    Create hola_mundo.py
    Program which print the classic message: Hello World

---
+++ PythonGeneral/hola_mundo/hola_mundo.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+"""Program which print the classic message: Hello World"""
+
+print("\n------------------")
+print("\nHello World!!!\n")
+print("\n¡¡¡Hola Mundo!!!\n")
+print("\n------------------")

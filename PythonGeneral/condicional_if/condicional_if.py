3868f698e46f6b8d6a9814883a339c781129b098
José Edgardo Morales Barroso <joseedgardo2000@gmail.com>
2019-09-08 16:52:36 -0500

    update

--- PythonGeneral/condicional_if/condicional_if.py
+++ PythonGeneral/condicional_if/condicional_if.py
@@ -12,6 +12,13 @@ value = input("Introduce a value: ")
 
 if value.isnumeric():
     print("The value '{}' is a number".format(value))
+    fvalue= float(value)
+    if fvalue < 25:
+        print("The value '{:.2f}' is smaller than 25".format(fvalue))
+    elif 25 <= fvalue <= 100:
+        print("The value {} is between 25 and 100".format(fvalue))
+    else:
+        print("The value {} is bigger than 100".format(fvalue))    
 else:
     print("The value '{}' is NOT a number".format(value))
 
